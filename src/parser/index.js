0c62e29cea337432f6b12b71c923c09ae1f699b0
Etienne Brodu <etnbrdcom@gmail.com>
2014-07-01 15:06:52 +0200

    parser

---
+++ src/parser/index.js
@@ -0,0 +1,16 @@
+var PEG = require("pegjs");
+var fs = require("fs");
+
+process.stdout.write("Generating parser ");
+var parser = PEG.buildParser("" + fs.readFileSync(__dirname + "/fluxion.peg"));
+console.log(" ... done.");
+
+var filename = process.argv[2];
+if (!filename) {
+  console.log("Please specify a filename as an argument");
+  return 0;
+}
+
+var res = parser.parse("" + fs.readFileSync(filename));
+
+console.log(res);
