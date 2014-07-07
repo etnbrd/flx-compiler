6ec5c6ef429068070f2d0eae7795f393d8d633da
Gautier DI FOLCO <gautier.difolco@gmail.com>
2014-07-07 11:09:03 +0200

    Improve test-tools

--- src/lib/traverse/test.js
+++ src/lib/traverse/test.js
@@ -1,32 +1,38 @@
 var t = require('../../../test/tools.js')
 ;
 
+function compileAndMock(filename) {
+    return require('supertest')(t.compileAndLoad(filename));
+}
+
 describe('Test Cases', function(){
     describe('IfStatement', function(){
         describe('outside of app.get', function(){
             it('if-then no else should compile', function(done){
-                t.compileAndMock('ifthen-out.js')
+                compileAndMock('ifthen-out.js')
                 .get('/')
                 .expect('42', done);
             })
 
             it('if-then-else should compile', function(done){
-                t.compileAndMock('ifthenelse-out.js')
+                compileAndMock('ifthenelse-out.js')
                 .get('/')
                 .expect('101010', done);
             })
         })
         describe('inside of app.get', function(){
             it('if-then no else should compile', function(done){
-                t.compileAndMock('ifthen-in.js')
+                compileAndMock('ifthen-in.js')
                 .get('/')
                 .expect('42', done);
             })
 
             it('if-then-else should compile', function(done){
-                t.compileAndMock('ifthenelse-in.js')
-                .get('/')
-                .expect('101010', done);
+                compileAndMock('ifthenelse-in.js')
+                    .get('/')
+                    .expect('101010')
+                    .end(done)
+                    ;
             })
         })
     })
--- test/tools.js
+++ test/tools.js
@@ -1,19 +1,23 @@
 var fs = require('fs')
-,   request = require('supertest')
 ,   t = require('../src/lib/tools')
 ,   compile = require('../src/compile');
 
 module.exports = {
     compile: compile
-,   load: load
+,   read: read
 ,   compileAndMock: compileAndMock
+,   compileAndLoad: compileAndLoad
 }
 
-function load(filename) {
+function read(filename) {
     return fs.readFileSync('./examples/' + filename).toString();
 }
 
+function compileAndLoad(filename) {
+    t.writeFile(filename, compile(read(filename)), __dirname + '/../results/');
+    return require(__dirname + '/../results/' + filename).app;
+}
+
 function compileAndMock(filename) {
-    t.writeFile(filename, compile(load(filename)), __dirname + '/../results/');
-    return request.agent(require(__dirname + '/../results/' + filename).app);
+    return require('supertest')(compileAndLoad(filename));
 }
