ea8373258b8dfd63e3fd66fe99a443d0be9966d8
Gautier DI FOLCO <gautier.difolco@gmail.com>
2014-07-08 11:07:29 +0200

    Refactoring src/linker/iterators/main.js

--- src/linker/iterators/main.js
+++ src/linker/iterators/main.js
@@ -1,28 +1,28 @@
-var _types = {}
+var bld = require('../../linker/builders');
+
+var _types = {};
 
 _types.Identifier = {
   enter: function(n) {
 
     if (n.modifier) {
-      if (n.modifier.target === "signature") {
+      if (n.modifier.target === 'signature') {
         return bld.signatureModifier(n.name);
       }
 
-      if (n.modifier.target === "scope") {
+      if (n.modifier.target === 'scope') {
         return bld.scopeModifier(n.name);
       }
     }
 
-    if (n.kind === "start"){
+    if (n.kind === 'start'){
       return bld.start(n.name.substring(1), n.signature);
     }
 
-    if (n.kind === "post"){
+    if (n.kind === 'post'){
       return bld.post(n.name.substring(1), n.signature);
     }
   }
-}
+};
 
 module.exports = _types;
-
-var bld = require('../../linker/builders');
