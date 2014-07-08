ec2ff39c9894e9bbcb6f2797c34440ad51ae8db8
Gautier DI FOLCO <gautier.difolco@gmail.com>
2014-07-08 11:07:29 +0200

    Refactoring src/pruner/index.js

--- src/pruner/index.js
+++ src/pruner/index.js
@@ -1,11 +1,10 @@
-var iterator = require("../lib/traverse").iterator(require('./iterators/main')),
-    cons = require("./constructors"),
-    map = require("../lib/traverse").map,
-    red = require("../lib/traverse").reduce;
+var iterator = require('../lib/traverse').iterator(require('./iterators/main')),
+    cons = require('./constructors'),
+    map = require('../lib/traverse').map;
 
 function start(ast) {
     var context = new cons.Context(ast);
-    context.enterFlx("Main", ast.program);
+    context.enterFlx('Main', ast.program);
     map(ast.program, iterator(context));
     context.leaveFlx();
     return context;
