c6ae2259c2aab60f85f0253cb98faab8204ab06a
Gautier DI FOLCO <gautier.difolco@gmail.com>
2014-07-08 11:29:58 +0200

    Fix src/linker/index.js

--- src/linker/index.js
+++ src/linker/index.js
@@ -7,42 +7,33 @@ function print(ast) {
     return _print(ast).code;
 }
 
-function printSig(flx) {
-    return flx.outputs.map(function (o) {
+function printFlx(flx) {
+    if (flx.outputs.length) {
+        return flx.outputs.map(function (o) {
             return o.name + ' [' + Object.keys(o.signature) + ']';
         }).join(', ');
+    }
+    else {
+        return 'ø';
+    }
 }
 
 function link(ctx) {
-    var index,
-        ast,
-        code,
-        flx,
-        length = ctx._flx.length,
-        _code;
 
     // Add the flx library
     ctx.ast.program.body.unshift(bld.requireflx());
-    ast = map(ctx._flx.Main.ast, iterator());
+    var ast = map(ctx._flx.Main.ast, iterator());
 
-    code = print(ast);
+    var code = print(ast);
 
-    for (index = 0; index < length; index++) {
-        flx = ctx._flx[index];
+    for (var _flx in ctx._flx) {
+        var flx = ctx._flx[_flx];
         if (flx.name !== 'Main') {
 
-            _code = print(bld.register(flx.name, map(flx.ast, iterator()), flx.scope));
+            var _code = print(bld.register(flx.name, map(flx.ast, iterator()), flx.scope));
 
             // This is only the comment :
-            code += "\n\n// " + flx.name + ' >> ';
-            if (flx.outputs.length) {
-                code += printSig(flx);
-            }
-            else {
-                code += 'ø';
-            }
-
-            code += "\n\n" + _code;
+            code += '\n\n// ' + flx.name + ' >> ' + printFlx(flx) + '\n\n' + _code;
         }
     }
 
