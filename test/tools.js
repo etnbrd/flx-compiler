bbb0f68f76535215759ccb88208aae06679a66f7
Gautier DI FOLCO <gautier.difolco@gmail.com>
2014-07-10 15:08:14 +0200

    Add custom linter

--- test/tools.js
+++ test/tools.js
@@ -1,6 +1,9 @@
+require('coffee-script/register');
 var fs = require('fs'),
     t = require('../src/lib/tools'),
-    compile = require('../src/compile');
+    compile = require('../src/compile'),
+    lint = require('./lint.coffee'),
+    assert = require('assert');
 
 function read(filename) {
     return fs.readFileSync('./examples/' + filename).toString();
@@ -8,6 +11,9 @@ function read(filename) {
 
 function compileAndLoad(filename) {
     var src = compile(read(filename));
+    var l = lint(src);
+    if (l.length)
+        assert.fail(false, true, 'Des variables globales sont référencez dans des fluxions :\n' + JSON.stringify(l, null, '\t'));
     t.writeFile(filename, src, __dirname + '/../results/');
     return require(__dirname + '/../results/' + filename).app;
 }
