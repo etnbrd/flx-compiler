36cd058475b5b8bf01efbb0db7b29ed584bcd9dd
Gautier DI FOLCO <gautier.difolco@gmail.com>
2014-07-11 09:37:28 +0200

    Always write compiled file, even if the lint fails

--- test/tools.js
+++ test/tools.js
@@ -11,10 +11,10 @@ function read(filename) {
 
 function compileAndLoad(filename) {
     var src = compile(read(filename));
+    t.writeFile(filename, src, __dirname + '/../results/');
     var l = lint(src);
     if (l.length)
         assert.fail(false, true, 'Des variables globales sont référencez dans des fluxions :\n' + JSON.stringify(l, null, '\t'));
-    t.writeFile(filename, src, __dirname + '/../results/');
     return require(__dirname + '/../results/' + filename).app;
 }
 
