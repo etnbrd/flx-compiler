02bcda78547d8c71937eb1cfc9a933c8ba7f5060
Etienne Brodu <etnbrdcom@gmail.com>
2014-07-23 15:43:53 +0200

    docs

--- gruntfile.js
+++ gruntfile.js
@@ -40,9 +40,8 @@ module.exports = function(grunt) {
     docco: {
       debug: {
         src: [
-          // 'lib/index.js',
-          // 'lib/linker/index.js',
           'lib/linker/core.js',
+          'lib/pruner/context.js'
         ],
         options: {
           output: 'docs',
@@ -50,19 +49,69 @@ module.exports = function(grunt) {
         }
       }
     },
+
+
+    'gh-pages': {
+      options: {
+        branch: 'gh-pages',
+        base: 'docs'
+      },
+      deploy: {
+        options: {
+          user: {
+            name: 'gh-pages',
+            email: 'etn@etnbrd.com'
+          },
+          repo: 'https://' + process.env.GH_TOKEN + '@github.com/etnbrd/flx-compiler.git',
+          message: 'publish gh-pages (auto)' + getDeployMessage(),
+          silent: true
+        },
+        src: ['**/*']
+      }
+    }    
   });
+
+
+  function getDeployMessage() {
+    var ret = '\n\n';
+    if (process.env.TRAVIS !== 'true') {
+      ret += 'missing env vars for travis-ci';
+      return ret;
+    }
+    ret += 'branch: ' + process.env.TRAVIS_BRANCH + '\n';
+    ret += 'SHA: ' + process.env.TRAVIS_COMMIT + '\n';
+    ret += 'range SHA: ' + process.env.TRAVIS_COMMIT_RANGE + '\n';
+    ret += 'build id: ' + process.env.TRAVIS_BUILD_ID + '\n';
+    ret += 'build number: ' + process.env.TRAVIS_BUILD_NUMBER + '\n';
+    return ret;
+  }
+
+  function deploy(){
+    if (process.env.TRAVIS === 'true' && process.env.TRAVIS_SECURE_ENV_VARS === 'true' && process.env.TRAVIS_PULL_REQUEST === 'false') {
+      grunt.log.writeln('executing deployment');
+      grunt.task.run('gh-pages:deploy');
+    } else {
+      grunt.log.writeln('skipped deployment');
+    }
+  }
 
   grunt.loadNpmTasks('grunt-simple-mocha');
   grunt.loadNpmTasks('grunt-env');
   grunt.loadNpmTasks('grunt-mkdir');
   grunt.loadNpmTasks('grunt-docco');
   grunt.loadNpmTasks('grunt-codeclimate');
+  grunt.loadNpmTasks('grunt-gh-pages');
+
+  grunt.registerTask('deploy', deploy);
+
+  grunt.registerTask('doc', ['docco', 'deploy']);
 
   if (process.env.TRAVIS) {
-    grunt.registerTask('test', ['mkdir:results' ,'env:coverage', 'simplemocha', 'codeclimate']);
+    grunt.registerTask('test', ['mkdir:results' ,'env:coverage', 'simplemocha', 'codeclimate', 'doc']);
   } else {
     grunt.registerTask('test', ['mkdir:results' ,'simplemocha']);
   }
 
-  grunt.registerTask('default', 'test');
+
+  grunt.registerTask('default', ['test', 'doc']);
 };
