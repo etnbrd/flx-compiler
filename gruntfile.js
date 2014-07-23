d2626d480ac1b21ab6373a70c93321bc70d87016
Etienne Brodu <etnbrdcom@gmail.com>
2014-07-23 10:17:21 +0200

    docco

--- gruntfile.js
+++ gruntfile.js
@@ -35,12 +35,27 @@ module.exports = function(grunt) {
         file: 'results/lcov.info',
         token: process.env.CODECLIMATE_REPO_TOKEN
       }
-    }
+    },
+
+    docco: {
+      debug: {
+        src: [
+          // 'lib/index.js',
+          // 'lib/linker/index.js',
+          'lib/linker/core.js',
+        ],
+        options: {
+          output: 'docs',
+          layout: 'parallel'
+        }
+      }
+    },
   });
 
   grunt.loadNpmTasks('grunt-simple-mocha');
   grunt.loadNpmTasks('grunt-env');
   grunt.loadNpmTasks('grunt-mkdir');
+  grunt.loadNpmTasks('grunt-docco');
   grunt.loadNpmTasks('grunt-codeclimate');
 
   if (process.env.TRAVIS)
